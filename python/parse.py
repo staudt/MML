d56286a63d354c71bc30d19a3c6b04c1dfedec34
staudt <ricardost@gmail.com>
2015-06-21 20:41:22 -0300

    Improvements

--- python/parse.py
+++ python/parse.py
@@ -2,23 +2,25 @@
 # -*- coding: utf-8 -*-
 
 class state:
-    IDENT, TAG, ATTRIBUTES, VALUE = range(4)
+    IDENTATION, TAG, ATTRIBUTES, VALUE = range(4)
 
 with open('../samples/test.mml', 'rb') as f:
     content = f.read()
 
 for line in iter(content.splitlines()):
-    expect = state.IDENT
-    parsed_line = { 'ident': '', 'tag': '', 'attributes': {}, 'value': '' }
+    expect = state.IDENTATION
+    parsed_line = { 'ident': '', 'tag': '', 'attributes': {}, 'value': '', 'children': [] }
     for c in range(len(line)):
-        if line[c] == '#':
-            break
-        if expect == state.IDENT:
+        if expect == state.IDENTATION:
+           if line[c] == '#':
+                break
            if line[c] in [' ', '\t']:
                 parsed_line['ident'] = '%s%s' % (parsed_line['ident'], line[c])
            else:
                 expect = state.TAG
         if expect == state.TAG:
+            if line[c] == '#':
+                break
             if line[c] == '(':
                 expect = state.ATTRIBUTES
             elif line[c] == ' ':
@@ -26,9 +28,16 @@ for line in iter(content.splitlines()):
             else:
                 parsed_line['tag'] = '%s%s' % (parsed_line['tag'], line[c])
         elif expect == state.ATTRIBUTES:
+            if line[c] == '#':
+                #fail
+                pass
             if line[c] == ')':
                 expect = state.VALUE
-            # not implemented yet
+            else:
+                pass
+                # attr parse not yet implemented implemented yet
         elif expect == state.VALUE:
+            if line[c] == '#':
+                break
             parsed_line['value'] = '%s%s' % (parsed_line['value'], line[c])
     print parsed_line
